95260b9f5a760f7fee2ba647512686279531cd49
cosmiccactus1 <tarkun08@gmail.com>
2024-10-06 21:45:43 +0200

    Update product.js

--- js/product.js
+++ js/product.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const mainImage = document.querySelector('.main-image');
+    const thumbnails = document.querySelectorAll('.thumbnail');
 
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', function() {
+            mainImage.src = this.src;
+            thumbnails.forEach(thumb => thumb.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+});
