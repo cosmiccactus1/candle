063d8b96efe7d6b97f93d4dd032bfa731599008a
cosmiccactus1 <tarkun08@gmail.com>
2024-12-17 15:52:43 +0100

    Update favoriti.js

--- js/favoriti.js
+++ js/favoriti.js
@@ -1,19 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const favoriteList = document.getElementById('favorite-list');
-    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
-    if (favorites.length > 0) {
-        favoriteList.innerHTML = '';
-        favorites.forEach(item => {
-            const favoriteItem = document.createElement('div');
-            favoriteItem.classList.add('favorite-item');
-            favoriteItem.innerHTML = `
-                <img src="${item.image}" alt="${item.name}" width="100">
-                <p>${item.name}</p>
-            `;
-            favoriteList.appendChild(favoriteItem);
-        });
-    } else {
-        favoriteList.innerHTML = `<p>Još nemate omiljenih stavki.</p>`;
-    }
-});
