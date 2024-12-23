3f7dda0d9517aa9baed06e4a0b2fce77d1b1ee81
cosmiccactus1 <tarkun08@gmail.com>
2024-12-23 14:03:07 +0100

    Update newsletter.js

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,1 +1,25 @@
-
+document.getElementById('newsletterForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const email = document.getElementById('newsletterEmail').value;
+    
+    fetch('newsletter.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+        if (data.success) {
+            document.getElementById('newsletterEmail').value = '';
+        }
+    })
+    .catch(error => {
+        alert('Došlo je do greške pri prijavi na newsletter');
+    });
+});
